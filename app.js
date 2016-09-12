c6dea8ecf3bdb8a3d0b6706fc155d74ce2d24591
Akhilesh N <akhilesh.reddy.03@gmail.com>
2016-09-12 11:36:10 +0530

    Commented mongoose code

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ var mongoose = require("mongoose");
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    //mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 
-mongoose.connect(mongoURL);
+//mongoose.connect(mongoURL);
 
-var campgroundSchema = new mongoose.Schema({
+/*var campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String
-});
+});*/
 
 
 app.set("view engine","ejs");
@@ -27,7 +27,7 @@ app.get("/about", function(req,res){
   res.render("about");
 });
 
-var Campground = mongoose.model("Campground", campgroundSchema);
+/*var Campground = mongoose.model("Campground", campgroundSchema);
 
 
 var newCampground = {name: "Sample1", image: "Image1", description: "Description1"};
@@ -38,7 +38,7 @@ Campground.create(newCampground,function(error,newlyCreated){
   } else {
     res.render("about");
   }
-});
+});*/
 
 
 //app.listen(process.env.PORT, process.env.IP);
