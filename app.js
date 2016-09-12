8ec11c58ae31e4e03516585c4711d82d711b4bb8
Akhilesh N <akhilesh.reddy.03@gmail.com>
2016-09-12 11:30:43 +0530

    Added mongoose

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+
+mongoose.connect(mongoURL);
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
 
 app.set("view engine","ejs");
 
@@ -13,6 +26,20 @@ app.get("/", function(req,res){
 app.get("/about", function(req,res){
   res.render("about");
 });
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+
+var newCampground = {name: "Sample1", image: "Image1", description: "Description1"};
+
+Campground.create(newCampground,function(error,newlyCreated){
+  if(error){
+    res.render("home");
+  } else {
+    res.render("about");
+  }
+});
+
 
 //app.listen(process.env.PORT, process.env.IP);
 
